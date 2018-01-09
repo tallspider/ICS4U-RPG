86319e7347337789185aec2e6a6232083f5751a0
ShigureKaiNi <34349533+ShigureKaiNi@users.noreply.github.com>
2018-01-09 14:41:46 -0500

    Update Fleet.java

--- Fleet.java
+++ Fleet.java
@@ -1,8 +1,16 @@
+/*
+Class Name: Fleet
+Author: Chongpu Zhao
+Date: January 8, 2018
+Purpose: Used to keep track the ships that in the battle part.
+*/
 public class Fleet{
 	final	private int MAX_SHIP = 10;
 	private Ship[] ships = new Ship [MAX_SHIP];
 	
 	public boolean addShip(int index,Ship newship){
+	/*add a ship to the array  with the given index
+	If ships[index] already has a object, return false. Otherwise return true.*/
 		if(!ships[index].equals(null)){
 			return false;
 		}else{
@@ -12,6 +20,8 @@ public class Fleet{
 	}
 	
 	public boolean deleteShip(int index){
+	/*Delete a ship in the given index
+	If the slot has no object, retutn false. Otherwise, return true*/
 		if(ships[index].equals(null)){
 			return false;
 		}else{
@@ -21,6 +31,7 @@ public class Fleet{
 	}
 	
 	public boolean checkEmpty(){
+	//Return true if the array is empty. Otherwise return false.
 		boolean toReturn = true;
 		for(int i = 0; i <MAX_SHIP;i++){
 			if(!ships[i].equals(null)){
@@ -30,6 +41,19 @@ public class Fleet{
 		return toReturn;
 	}
 	public Ship getShip(int index){
+	//Return the ship object in the array of the given index.
 		return ships[index];
+	}
+	
+	public Ship[] getFleet(){
+	//Return the array of the ships.
+		return ships;
+	}	
+	
+	public void emptyFleet(){
+	//Delete all the object in the fleet array.
+		for(int i = 0; i <MAX_SHIP;i++){
+			ships[i] = null;
+		}
 	}
 }
