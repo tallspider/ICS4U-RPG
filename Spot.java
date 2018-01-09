7a23501244c937fcf03a37962d5f6e2a9f1dd3a6
Fl47 <34190603+Fl47@users.noreply.github.com>
2018-01-08 23:52:20 -0500

    Add files via upload

---
+++ Spot.java
@@ -0,0 +1,74 @@
+/*
+Note to self:
+
+   -class headers indicating:
+o class name
+o author
+o date
+
+o school
+o purpose
+   -method headers including:
+o description of each parameters and return values
+o purpose
+   -sufficient and appropriate comments describing
+o code that is difficult to understand
+o constant, type and variable declarations
+*/
+
+/*
+Class Name: Spot
+Author: David Qian 
+Date: January 7, 2018
+School: A.Y.Jackson S.S
+Purpose: Retain the information regarding every square / spot in the map
+*/
+
+public class Spot{
+
+// the thing or entity that can be held withing the spot
+   Entity thing;
+
+// empty constructor
+   public Spot(){
+   }
+
+//accessors and mutators
+   public void setWall(){
+      this.thing = new Wall();
+   }
+
+   public void setEmpty(){
+      this.thing = null;
+   }
+
+   public void setShip(Ship ship){
+      this.thing = ship;
+   }
+
+   public boolean isEmpty(){
+      if (this.thing == null){
+         return true;
+      }
+      return false;
+   }
+   
+   public boolean isWall(){
+      if (this.thing instanceof Wall){
+         return true;
+      }
+      return false;
+   }
+
+   public boolean isShip(){
+      if (this.thing instanceof Ship){
+         return true;
+      }
+      return false;
+   }
+   
+   // isShip will be used with getShip to ensure this.thing is a ship
+   public Ship getShip(){
+      return (Ship)this.thing;
+   }   
+}
---
+++ Wall.java
@@ -0,0 +1,32 @@
+/*
+Note to self:
+
+   -class headers indicating:
+o class name
+o author
+o date
+
+o school
+o purpose
+   -method headers including:
+o description of each parameters and return values
+o purpose
+   -sufficient and appropriate comments describing
+o code that is difficult to understand
+o constant, type and variable declarations
+*/
+
+/*
+Class Name: Entity
+Author: David Qian 
+Date: January 7, 2018
+School: A.Y.Jackson S.S
+Purpose: A wall that is a entity that is used in movement calculation (obstructs movement)
+*/
+
+public class Wall extends Entity{
+
+//constructor
+   public Wall(){
+   }
+}
