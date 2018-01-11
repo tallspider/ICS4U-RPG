023aa836f4f4f76bc0439094ed313b2b97b81467
Fl47 <34190603+Fl47@users.noreply.github.com>
2018-01-11 14:45:24 -0500

    Add files via upload

--- Map.java
+++ Map.java
@@ -33,6 +33,11 @@ Purpose: Used to keep track of entities (ships and walls)
    //********************************************************************TEMPORARY WORKAROUND **********************************************************************//
       public static int WIDTH_OF_MAP = 16;
       public static int LENGTH_OF_MAP = 16;
+      
+   // to determine result of game for checkEnd
+      public static final int NOT_END = 0;
+      public static final int USER_WINS = 1;
+      public static final int AI_WINS = 2;
    
    // the board is a 2d spot array
       Spot[][] board;
@@ -240,6 +245,36 @@ Purpose: Used to keep track of entities (ships and walls)
          return this.board;
       }
    
+       public int checkEnd(){
+       
+         boolean userHasShips = false;
+         boolean aiHasShips = false;
+       
+         for (int x = 0 ; x < board.length ; x++){
+            for (int y = 0 ; y < board[x].length ; y++){
+               if (new Location(x,y).isShip()){
+                  if(new Location(x,y).getShip().ownedByPlayer()){
+                     userHasShips = true;
+                  }
+                  else{
+                     aiHasShips = true;
+                  }
+               }
+            }
+         }
+         
+         if (userHasShips && aiHasShips){
+            return NOT_END;
+         }
+         else if (userHasShips && !aiHasShips){
+            return USER_WINS;
+         }
+         else if (!userHasShips && aiHasShips){
+            return AI_WINS;
+         }
+         System.out.println("Error: Both teams defeated");
+         return -1;
+      }
    //********************SUBJECT TO CHANGE*************************** USED TO TEST/DEBUG****************
        public void display(){
       
