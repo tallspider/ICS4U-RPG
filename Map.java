e92c51d6f8dfc3de2c53d45dd5c93f0ab96159e7
Fl47 <34190603+Fl47@users.noreply.github.com>
2018-01-12 13:48:54 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -212,12 +212,12 @@ Purpose: Used to keep track of entities (ships and walls)
          addRandomEntity(ship);
       }
    
+   // loops through the entire board and finds the empty space nearest to the target (bottom Right)
        public void addShipBottomRight(Ship ship){
       
          Location closest;
          Location bottomRight = new Location(WIDTH_OF_MAP,0);  
-      	
-         // loops through the entire board and finds the empty space nearest to the target (bottom Right)
+
          for (int x = 0 ; x < board.length ; x ++){
             for (int y = 0 ; y < board[x].length ; y++){
             
