3feeae2ae9412d0b7a3829c49ead4c631f63bcfb
ShigureKaiNi <34349533+ShigureKaiNi@users.noreply.github.com>
2018-01-12 13:56:41 -0500

    Update RPGDemo.java

--- RPGDemo.java
+++ RPGDemo.java
@@ -6,6 +6,7 @@
 	 	private static TextField loginField = new TextField(10);
 		private static TextField passwordField = new TextField(10);  		  	
 	 	private static Login user; 
+		private static ErrorFrame errorFrame = new ErrorFrame();
        public static void main(String[]args){
 
 			 MainFrame mainFrame =new MainFrame("RPG");
@@ -31,19 +32,39 @@
             pwp.add(new JLabel("Password"));
             pwp.add(passwordField);
             this.add(pwp);
-				Button LogInB = new Button("Login");
+				Button LogInB = new Button("Login"); 
             this.add(LogInB);
             this.add(new Button("Password"));
 				LogInB.addMouseListener(new TextListener ());
          }
       }
 		
-		   
+		static class ErrorFrame extends JFrame{
+				public ErrorFrame() {
+				 this.setLayout(new FlowLayout());
+				this.setBounds(600,600,150,100);
+				this.setVisible(false);
+				JLabel error = new JLabel("Error");
+			//	error.setFont(new Font("Serif",Font.BOLD,20));
+				this.add(error);
+				Button confirm = new Button("Confirm");
+				this.add(confirm);
+				confirm.addMouseListener(new ConfirmListener());
+				}
+			}
+   	 static class ConfirmListener extends MouseAdapter{
+		 	public void mouseClicked(MouseEvent e){
+				errorFrame.setVisible(false);
+				
+				errorFrame.repaint();
+			}
+		 }
        static class TextListener extends MouseAdapter{
           public void mouseClicked(MouseEvent e){
 				String username = loginField.getText();
 				String password = passwordField.getText();
 				user = new Login(username,password);
+				errorFrame.setVisible(true);
          }    	
       }
    }
