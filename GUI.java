c7a41cd7a5783d9f85ba702370c09c625d482da1
tallspider <annie.gao2@student.tdsb.on.ca>
2018-01-12 21:43:41 -0500

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,157 @@
+   import javax.swing.*;
+   import java.awt.*;
+   import java.awt.event.*;
+
+    public class GUI extends Frame implements MouseListener 
+   {
+   
+      private int y = e.getY();
+      private int x = e.getX();
+   
+    	
+    
+    
+    
+    
+    
+    
+    
+       public void mouseClicked(MouseEvent e) 
+      {  
+         Location.setX(x);
+         Location.setY(y);
+      
+      
+      
+      
+      }  
+   	
+   	
+       public void mouseEntered(MouseEvent e)
+      {  
+         Location.setX(x);
+         Location.setY(y);
+      
+      
+      
+      }  
+   	
+   	
+       public void mouseExited(MouseEvent e)
+      {  
+         Location.setX(x);
+         Location.setY(y);
+      
+      
+      
+      
+      }  
+   	
+   	
+       public void mousePressed(MouseEvent e) 
+      {  
+         Location.setX(x);
+         Location.setY(y);
+      
+      
+      
+      
+      }  
+   	
+   	
+       public void mouseReleased(MouseEvent e)
+      {  
+         Location.setX(x);
+         Location.setY(y);
+      
+      
+      
+      	      
+      }  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   /*       class Drawing extends JComponent
+      {
+          public void paint(Graphics g)
+         {
+            if (menu)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            }
+            
+            else if (fleet)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            
+            }
+            
+            
+            else if (battle)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            
+            }
+            
+            
+            else if (leaderBoard)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            }
+            
+            else if (login)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            
+            }
+            
+            else if (save)
+            {
+               int y = e.getY();
+               int x = e.getX();
+            
+            
+            
+            }
+                           
+         }
+      }
+    
+   */
+   
+   
+   
+   
+   
+   
+   
+   }  
+
+
+
+
+
+
+
