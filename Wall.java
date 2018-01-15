ebe5823c0a0e42ebbbf0deab6e236a603d6ef6a3
Fl47 <34190603+Fl47@users.noreply.github.com>
2018-01-14 21:59:52 -0500

    Add files via upload

--- Wall.java
+++ Wall.java
@@ -1,23 +1,5 @@
 /*
-Note to self:
-
-   -class headers indicating:
-o class name
-o author
-o date
-
-o school
-o purpose
-   -method headers including:
-o description of each parameters and return values
-o purpose
-   -sufficient and appropriate comments describing
-o code that is difficult to understand
-o constant, type and variable declarations
-*/
-
-/*
-Class Name: Entity
+Class Name: Wall
 Author: David Qian 
 Date: January 7, 2018
 School: A.Y.Jackson S.S
@@ -26,7 +8,13 @@ Purpose: A wall that is a entity that is used in movement calculation (obstructs
 
 public class Wall extends Entity{
 
+   public final static String imageFile = "wall.png";
+
 //constructor
    public Wall(){
+   }
+   
+   public String getImageFile(){
+      return imageFile;
    }
 }
