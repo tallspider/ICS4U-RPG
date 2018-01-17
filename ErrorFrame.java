f45a3060344f84e0c4434057b6b3fe13075f88c3
ShigureKaiNi <34349533+ShigureKaiNi@users.noreply.github.com>
2018-01-17 14:07:44 -0500

    Update ErrorFrame.java

--- ErrorFrame.java
+++ ErrorFrame.java
@@ -3,18 +3,27 @@ import java.awt.*;
 import java.awt.event.*;
 public class ErrorFrame extends JFrame{
    public static JLabel error = new JLabel("Error");
+   private int length;
+   
    public ErrorFrame() {
+	   super("Error");
+	   length = error.getText().length();
       this.setLayout(new FlowLayout());
-      this.setBounds(600,600,150,100);
+      this.setBounds(600,600,length*30,300);
+      error.setFont(new Font("TimesRoman", Font.BOLD, 25));
+      error.setPreferredSize(new Dimension(length*30,150));
       this.setVisible(true);
       //JLabel error = new JLabel("Error");
       //error.setFont(new Font("Serif",Font.BOLD,20));
       this.add(error);
       Button confirm = new Button("Confirm");
+      confirm.setFont(new Font("TimesRoman", Font.BOLD, 20));
+      confirm.setPreferredSize(new Dimension(150,50));
       this.add(confirm);
       confirm.addMouseListener(new ConfirmListener(this));
    }
-   static class ConfirmListener extends MouseAdapter{
+   
+   private class ConfirmListener extends MouseAdapter{
       ErrorFrame ef;
       
       public ConfirmListener(ErrorFrame eF){
@@ -24,6 +33,10 @@ public class ErrorFrame extends JFrame{
          ef.setVisible(false); 		
          //errorFrame.repaint();
       }
+   }
+   
+   public static void setText(String s) {
+	   error = new JLabel(s,SwingConstants.CENTER);
    }
    
 }
