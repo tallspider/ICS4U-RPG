ee5be124b9e208c7d02396f128094aee5cd36c69
tallspider <annie.gao2@student.tdsb.on.ca>
2018-01-22 12:53:30 -0500

    Update HangarFrame.java

--- HangarFrame.java
+++ HangarFrame.java
@@ -799,7 +799,7 @@ class HangarInfoBotMid extends JPanel{
                   if(!hangar.getShipUpgradable(shipID)){
                      hangarFrame.showErrorMessage("You cannot upgrade this ship anymore.", "No More Upgrades!");
                   }
-                  else if(player.getNumCoins() < hangar.getShipUpgradeCost(shipID, Ship.TR_Upgrade)){
+                  else if(player.getNumCoins() < hangar.getShipUpgradeCost(shipID, Ship.AR_Upgrade)){
                      hangarFrame.showErrorMessage("Not enough money to upgrade!", "Not Enough Money!");
                   }
                   else if(hangarFrame.askQuestion("Are you sure you would like to upgrade this ship?", "Sure?")){
