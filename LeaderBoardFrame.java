e3e7882a36afbec2d1bfa4fe3a2bcebbfa2d2f3d
Hans Long <34349524+lyq11131@users.noreply.github.com>
2018-01-22 01:08:19 -0500

    Update LeaderBoardFrame.java

--- LeaderBoardFrame.java
+++ LeaderBoardFrame.java
@@ -1,9 +1,17 @@
+//Class name: LeaderBoardFrame
+//Author: Hans Long
+//Date: Jan. 12, 2018
+//School: A.Y.Jackson S.S.
+//Purpose: the graphical representation of the LeaderBoardFrame class
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JComponent;
-public class LeaderBoardFrame extends JFrame{
 
+public class LeaderBoardFrame extends JFrame
+{
    private JLabel wayOfSort;
    private LeaderBoard ranking;
    private static int num;
@@ -12,44 +20,51 @@ public class LeaderBoardFrame extends JFrame{
    private static JLabel[] score;
    private static JPanel[] sortByScore;
    private static JPanel[] sortByName;
-   // private static JPanel displaypart;
+   //two different display method
    private static ComparisonPlayer[] nameSort;
    private static ComparisonPlayer[] scoreSort;
-   private static Button change1 = new Button("Switch to sort by name");
-   private static Button change2 = new Button("Switch to sort by player"); 
+	
+   //set the panel that contains all player's score informational panels
    private JPanel p = new JPanel();
    private String leaderBoardname;
 	
 
-   
+    //constructor of the LeaderBoardFrame class
    public LeaderBoardFrame(String un, MainScene ms)
    {
       ms.addReopenListener(this);
       this.setTitle("LeaderBoard");
+      //set the handout of the center part
       this.leaderBoardname = un;
       wayOfSort = new JLabel("Rank   Player   Score", SwingConstants.CENTER);
+      //set the font's kind and size
       wayOfSort.setFont(new Font("TimesRoman", Font.BOLD, 15));
       ranking = new LeaderBoard(leaderBoardname);
       scoreSort = ranking.getPlayersByScore();
       nameSort = ranking.getPlayersByName();
       num = nameSort.length;
       
+      //set the layout of the frame 
+      //there are five pixels between the vertical components of the frame
       this.setLayout(new BorderLayout(0,5));
+      //set the prefer size of the head
       wayOfSort.setPreferredSize(new Dimension(400, 15));
-      // wayOfSort.setBackground(Color.YELLOW);
       
+      //set the head's place
       this.add(wayOfSort,BorderLayout.NORTH);
       this.setResizable(false);
       this.setBounds(500,200,400,num*30+150);
       this.setVisible(true);
       
+      //set the layout of the panel to null
       p.setLayout(null);
       
+      //set the length of the arrays
       rank = new JLabel[num];
       names = new JLabel[num];
       score = new JLabel[num];
       
-      
+      //assign values to the arrays
       for(int i = 0;i<num;i++)
       {
          rank[i] = new JLabel(" "+Integer.toString(i+1)+"       ");
@@ -57,8 +72,12 @@ public class LeaderBoardFrame extends JFrame{
          score[i] = new JLabel(Integer.toString(scoreSort[i].getScore())+"        "); 
       }
       
+      //set the length of the panel array
       sortByScore = new JPanel[num];
       
+      
+      //add each player's information into the sortByName2 panel
+      //set the layout of each panel
       for(int i = 0; i<num; i++)
       {      
          sortByScore[i] = new JPanel();
@@ -67,12 +86,14 @@ public class LeaderBoardFrame extends JFrame{
          sortByScore[i].add(score[i]); 
          sortByScore[i].setLayout(new FlowLayout());
       }
-      
-      
+       
+      //add all players' information to p2
       for(int i = 0; i<num; i++)
       {
          System.out.println(i+1);
          sortByScore[i].setBounds(10,10+i*35,400,30);
+	 
+	 //set the background color of the personal information 
          if (i%2==0)
          {
             sortByScore[i].setBackground(Color.RED);
@@ -89,19 +110,25 @@ public class LeaderBoardFrame extends JFrame{
       getContentPane().add("Center",p); 
       p.setVisible(true);
    
-       
+      
+      //create the button to let player the change the sort way 
       Button change = new Button("Switch to sort by name");
       change.setFont(new Font("TimesRoman", Font.BOLD, 15));
       add(change,BorderLayout.SOUTH);
+      //check if the window was click by the player
       addWindowListener(new MyWindowMonitor());
       
+      
+      //add a minotor to get the access information of the button 
+      //when palyer click the button it will change the way of sort
       Monitor mo = new Monitor(this,leaderBoardname);
       change.addActionListener(mo);
-      //change.addMouseListener(new SortScoreListener(this));
       
           
    }
    
+   
+   //a class to close the window if player click the close button
    class MyWindowMonitor extends WindowAdapter
    {
       public void windowclosing(WindowEvent e)
@@ -111,17 +138,23 @@ public class LeaderBoardFrame extends JFrame{
       }
    }
    
+   
+   //a monitor class to check the palyer's action of this button
    class Monitor implements ActionListener
    {
       private LeaderBoardFrame lf = null;
       private String un2 = leaderBoardname;
       
+      //constructor of the class
+      //pass the LeaderBoardFrame2
       public Monitor(LeaderBoardFrame fr,String leaderBoardname)
       {
          this.lf = fr;
          this.un2 = leaderBoardname;
       }
       
+      //set the way of sort by score unvisible
+      //set the way of sort by name visible
       public void actionPerformed(ActionEvent e)
       {
          lf.setVisible(false);
@@ -186,80 +219,4 @@ public class LeaderBoardFrame extends JFrame{
    }      
    
    
-   
-   // class SortScoreListener extends MouseAdapter
-   // {  
-      // LeaderBoardFrame lbf = null;
-   //    
-      // public SortScoreListener(LeaderBoardFrame lbf)
-      // {
-         // this.lbf = lbf;
-      // }
-   //    
-      // public void mouseClicked(MouseEvent e)
-      // {
-         // lbf.removeAll();
-         // wayOfSort.setText("rank/nPlayer\nScore");
-      //    
-         // for(int i =0; i<nameSort.length;i++)
-         // {
-            // name[i] = new JLabel(nameSort[i].getUsername());
-            // score[i] = new JLabel(Integer.toString(nameSort[i].getScore())); 
-         // }
-      //    
-         // Button change = new Button("Switch to sort by name");
-         // lbf.add(change);
-         // change.addMouseListener(new SortScoreListener(lbf));
-         // lbf.addWindowListener
-            // (
-            // 
-            // new WindowAdapter()
-            // {
-               // public void windowClosing(WindowEvent e)
-               // {
-                  // setVisible(false);
-               // }
-            // });
-         // lbf.repaint();
-      // 
-      // }
-   //   
-   // }  
-//    
-   // class SortNameListener extends MouseAdapter
-   // {
-      // LeaderBoardFrame lbf = null;
-   //    
-      // public SortNameListener(LeaderBoardFrame lbf)
-      // {
-         // this.lbf = lbf;
-      // }
-      // public void mouseClicked(MouseEvent e)
-      // {
-         // lbf.removeAll();
-         // wayOfSort.setText("rank/tScore\tPlayer");
-      //    
-         // for (int i = 0;i<num;i++)
-         // {
-            // name[i] = new JLabel(scoreSort[i].getUsername());
-            // score[i] = new JLabel(Integer.toString(scoreSort[i].getScore())); 
-         // }
-      //    
-         // Button change = new Button("Switch to sort by name"); 
-         // lbf.add(change);
-         // change.addMouseListener(new SortNameListener(lbf));
-      //    
-         // lbf.addWindowListener
-            // (
-            // new WindowAdapter()
-            // {
-               // public void windowClosing(WindowEvent e)
-               // {
-                  // dispose();
-               // }
-            // });
-      // }
-   // }    
-//    
-//    
 }
